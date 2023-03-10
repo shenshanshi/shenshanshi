d74b7cacf7acbb47957c0a9f9961bce8f9a39b18
绪运 莫 <2265364722@qq.com>
2023-03-10 15:05:39 +0800

    添加swagger

--- shenshanshi-account/src/main/java/com/nico/account/AccountApplication.java
+++ shenshanshi-account/src/main/java/com/nico/account/AccountApplication.java
@@ -5,10 +5,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(scanBasePackages = "com.nico")
 @EnableDiscoveryClient
 @MapperScan("com.nico.account.mapper")
+@EnableSwagger2
+
 public class AccountApplication {
 
      public static void main(String[] args) {
